90e2f2acc7f9c2890850c969b00ad44ebc152c2b
Monty-2003 <tmontgomery2021@gmail.com>
2024-08-19 21:24:54 -0500

    Authentication working - basic page setups

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='root_path'),
+    path('orders/orders.html', views.order_display, name='orders_path'),
+    path('products/products.html', views.product_display, name='products_path'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,23 @@
+from pyexpat.errors import messages
 from django.shortcuts import render
 import shopify
 from shopify_app.decorators import shop_login_required
 
+
 @shop_login_required
 def index(request):
-    products = shopify.Product.find(limit=3)
-    orders = shopify.Order.find(limit=3, order="created_at DESC")
+    
+    products = shopify.Product.find(order="name DESC")
+    orders = shopify.Order.find(order="created_at DESC")
     return render(request, 'home/index.html', {'products': products, 'orders': orders})
+
+@shop_login_required
+def product_display(request):
+    products = shopify.Product.find(order="name DESC")
+    return render(request, 'products/products.html', {'products': products})
+
+@shop_login_required
+def order_display(request):
+    orders = shopify.Order.find(order="created_at DESC")
+    return render(request, 'orders/orders.html', {'orders': orders})
+    
